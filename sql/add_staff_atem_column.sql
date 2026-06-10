2a4a694e6dd4376df493e688f966eea9836f57e8
Siti Anasuha <anasuharosli@gmail.com>
2026-06-10 12:41:29 +0800

    fix: add after column

--- sql/add_staff_atem_column.sql
+++ sql/add_staff_atem_column.sql
@@ -1,2 +1,2 @@
 ALTER TABLE staff
-    ADD COLUMN atem TINYINT(1) NOT NULL DEFAULT 0 COMMENT '0 = normal user, 1 = superadmin';
+    ADD COLUMN atem TINYINT(1) NOT NULL DEFAULT 0 COMMENT '0 = normal user, 1 = superadmin' AFTER coin_hub;
